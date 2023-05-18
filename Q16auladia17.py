5cc9b5d6cc02aa93162f661259f3ff52dbdcbf9b
M. Eduarda Souza <souza.eduarda2@escolar.ifrn.edu.br>
2023-05-18 10:30:24 -0300

    Q16auladia17.py
    programa que efetua as operações

---
+++ Q16auladia17.py
@@ -0,0 +1,7 @@
+print('Digite dois numeros')
+N1 = int(input())
+N2 = int(input())
+soma = N1 + N2
+sub = N1 - N2
+mult = N1 * N2
+print('a soma entre os numeros É {}, A subtração {}, a multiplicação {}'.format(soma, sub, mult))
