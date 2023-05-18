c8c4a5695ae222b8a89fa2bf5e0b13d649ba43ac
M. Eduarda Souza <souza.eduarda2@escolar.ifrn.edu.br>
2023-05-18 11:04:51 -0300

    Q18auladia17.py
    programa que efetua as operações de matemática básica

---
+++ Q18auladia17.py
@@ -0,0 +1,8 @@
+print('Digite dois numero')
+N1 = int(input())
+N2 = int(input())
+soma = N1 + N2
+sub = N1 - N2
+mult = N1 * N2
+div = N1 / N2
+print('A soma entre os numeros É {}, a subtração {}, a Multiplicaçaõ {}, a divisão {}'.format(soma,sub,mult, div))                                                  
