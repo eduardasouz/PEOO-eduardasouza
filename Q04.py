fef620fed5a21b0da95abffd53b063c9a02151b7
M. Eduarda Souza <souza.eduarda2@escolar.ifrn.edu.br>
2023-05-25 11:36:25 -0300

    Q04.py

---
+++ Q04.py
@@ -0,0 +1,21 @@
+print('digite 5 numeros:')
+n1 = float(input())
+n2 = float(input())
+n3 = float(input())
+n4 = float(input())
+n5 = float(input())
+
+cal1 = n1 // 1
+print('a media ponderada de 1Bim', cal1)
+cal2 = n2 // 2
+print('a media ponderada de 2Bim', cal2)
+cal3 = n3 // 3
+print('a media ponderada de 3Bim', cal3)
+cal4 = n4 // 4
+print('a media ponderada de 4Bim', cal4)
+cal5 = n5 // 5
+print('a media ponderada de 5Bim', cal5)
+
+
+
+
