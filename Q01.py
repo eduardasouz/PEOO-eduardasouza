f438587523e85f337c201b68aea4002acadf13eb
M. Eduarda Souza <souza.eduarda2@escolar.ifrn.edu.br>
2023-05-25 10:56:01 -0300

    Q01.py
    algoritmo

---
+++ Q01.py
@@ -0,0 +1,10 @@
+print('digite o valor de A:')
+A = int(input())
+print('digite o valor de B:')
+B = int(input())
+print('digite o valor de C:')
+C= float(input())
+
+cal = A**2 + B + C
+
+print('o resultado da equeção é {}'.format(cal))
