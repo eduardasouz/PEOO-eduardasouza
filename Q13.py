65b02d62514e1c9c559d07902655bd32c9c48bb8
M. Eduarda Souza <souza.eduarda2@escolar.ifrn.edu.br>
2023-05-25 11:44:38 -0300

    Q13.py

---
+++ Q13.py
@@ -0,0 +1,8 @@
+print('DIGITE A AULTURA DA LATA:')
+h = float(input())
+print('DIGITE O RAIO DA LATA:')
+r = float(input())
+
+v = 3,14 * r ** 2 * h
+
+print('o volume da lata de oleo é {}'.format(v))
